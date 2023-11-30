40d1b97f39b5ac760409e5d07c9e1cca6087e478
Arthur Malumian <amalumian@outlook.com>
2023-11-30 21:54:16 +0700

    Disable virtual text

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -4,7 +4,7 @@ return {
     opts = {
       diagnostics = {
         underline = true,
-        virtual_text = true,
+        virtual_text = false,
         float = {
           border = "rounded",
         },
