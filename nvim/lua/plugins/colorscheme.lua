f12187178e04415b7aa9facf0cad04e78096676a
Arthur Malumian <amalumian@outlook.com>
2024-09-11 02:17:19 +0700

    Update nvim config

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -11,4 +11,19 @@ return {
 			})
 		end,
 	},
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = {
+		transparent_background = true,
+	} },
+	{
+		"folke/tokyonight.nvim",
+		lazy = true,
+		opts = {
+			style = "night",
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+		},
+	},
 }
