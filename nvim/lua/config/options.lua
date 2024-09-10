4abd761e097c59e5dcc8f5edc115d8a625ec4691
Arthur Malumian <amalumian@outlook.com>
2024-09-10 21:58:53 +0700

    Update nvim config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -31,6 +31,7 @@ vim.opt.splitbelow = true -- Put new windows below current
 vim.opt.splitright = true -- Put new windows right of current
 vim.opt.splitkeep = "cursor"
 vim.opt.mouse = ""
+vim.opt.guicursor = ""
 vim.opt.termguicolors = true
 vim.opt.colorcolumn = "100"
 
