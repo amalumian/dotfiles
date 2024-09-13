eafe394588dbf6281632e3c4ec65b2fd1707abff
Arthur Malumian <amalumian@outlook.com>
2024-09-14 06:41:20 +0700

    Update nvim config

--- nvim/lua/amalumian/plugins/colorscheme.lua
+++ nvim/lua/amalumian/plugins/colorscheme.lua
@@ -1,16 +1,47 @@
 return {
-  'rose-pine/neovim',
-  priority = 1000, -- Make sure to load this before all the other start plugins.
-  name = 'rose-pine',
-  opts = {
-    styles = {
-      bold = true,
-      italic = true,
-      transparency = true,
-    },
+  {
+    'projekt0n/github-nvim-theme',
+    priority = 1000,
+    config = function()
+      require('github-theme').setup {
+        options = {
+          transparent = true,
+        },
+      }
+
+      vim.cmd 'colorscheme github_dark'
+    end,
   },
-  init = function()
-    vim.cmd.colorscheme 'rose-pine'
-    vim.cmd.hi 'Comment gui=none'
-  end,
+  -- {
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000,
+  --   opts = {
+  --     style = 'night',
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = 'transparent',
+  --       floats = 'transparent',
+  --     },
+  --   },
+  --   init = function()
+  --     vim.cmd.colorscheme 'tokyonight'
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
+  -- {
+  --   'rose-pine/neovim',
+  --   priority = 1000,
+  --   name = 'rose-pine',
+  --   opts = {
+  --     styles = {
+  --       bold = true,
+  --       italic = true,
+  --       transparency = true,
+  --     },
+  --   },
+  --   init = function()
+  --     vim.cmd.colorscheme 'rose-pine'
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
 }
