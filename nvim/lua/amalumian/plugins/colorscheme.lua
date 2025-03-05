c2bef475ff94189c9b933fd8edf5a0954763a088
Arthur Malumian <arthurmalumian@gmail.com>
2025-03-05 23:29:57 +0700

    Update nvim config

--- nvim/lua/amalumian/plugins/colorscheme.lua
+++ nvim/lua/amalumian/plugins/colorscheme.lua
@@ -19,47 +19,4 @@ return {
             ]]
     end,
   },
-  -- {
-  --   'catppuccin/nvim',
-  --   name = 'catppuccin',
-  --   priority = 1000,
-  --   opts = {
-  --     transparent_background = true,
-  --   },
-  --   init = function()
-  --     vim.cmd.colorscheme 'catppuccin-latte'
-  --   end,
-  -- },
-  -- {
-  --   'folke/tokyonight.nvim',
-  --   priority = 1000,
-  --   opts = {
-  --     style = 'night',
-  --     transparent = true,
-  --     styles = {
-  --       sidebars = 'transparent',
-  --       floats = 'transparent',
-  --     },
-  --   },
-  --   init = function()
-  --     vim.cmd.colorscheme 'tokyonight'
-  --     vim.cmd.hi 'Comment gui=none'
-  --   end,
-  -- },
-  -- {
-  --   'rose-pine/neovim',
-  --   priority = 1000,
-  --   name = 'rose-pine',
-  --   opts = {
-  --     styles = {
-  --       bold = true,
-  --       italic = true,
-  --       transparency = true,
-  --     },
-  --   },
-  --   init = function()
-  --     vim.cmd.colorscheme 'rose-pine'
-  --     vim.cmd.hi 'Comment gui=none'
-  --   end,
-  -- },
 }
