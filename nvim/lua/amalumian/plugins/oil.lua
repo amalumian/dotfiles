75175bdad19ccd102bd38447ec0b7a69a1f08e9b
Arthur Malumian <arthurmalumian@gmail.com>
2025-03-27 16:56:56 +0700

    Update nvim config

--- nvim/lua/amalumian/plugins/oil.lua
+++ nvim/lua/amalumian/plugins/oil.lua
@@ -3,6 +3,13 @@ return {
   ---@module 'oil'
   ---@type oil.SetupOpts
   opts = {
+    keymaps = {
+      ['<C-h>'] = false,
+      ['<C-l>'] = false,
+      ['<C-k>'] = false,
+      ['<C-j>'] = false,
+      ['<M-\\>'] = 'actions.select_split',
+    },
     view_options = {
       show_hidden = true,
     },
