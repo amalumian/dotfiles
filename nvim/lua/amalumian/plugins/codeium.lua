abfbc030a2ba22bb8ab794effb8d68b08cf3ce64
Arthur Malumian <arthurmalumian@gmail.com>
2025-04-26 20:31:35 +0700

    Update neovim config

---
+++ nvim/lua/amalumian/plugins/codeium.lua
@@ -0,0 +1,24 @@
+return {
+  'Exafunction/codeium.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'hrsh7th/nvim-cmp',
+  },
+  cmd = 'Codeium',
+  event = 'InsertEnter',
+  build = ':Codeium Auth',
+  opts = {
+    enable_cmp_source = false,
+    virtual_text = {
+      enabled = true,
+      idle_delay = 0,
+      key_bindings = {
+        accept = '<Tab>',
+        accept_word = false,
+        accept_line = '<C-\\>',
+        next = '<M-]>',
+        prev = '<M-[>',
+      },
+    },
+  },
+}
