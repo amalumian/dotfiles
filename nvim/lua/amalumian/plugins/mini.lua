1f372ee6ac682ec1364178af38d8d37051c439ac
Arthur Malumian <arthurmalumian@gmail.com>
2025-04-30 22:30:37 +0700

    Update Neovim config

--- nvim/lua/amalumian/plugins/mini.lua
+++ nvim/lua/amalumian/plugins/mini.lua
@@ -3,11 +3,6 @@ return {
   config = function()
     require('mini.ai').setup { n_lines = 500 }
     require('mini.surround').setup()
-    require('mini.animate').setup {
-      scroll = {
-        enable = false,
-      },
-    }
     require('mini.starter').setup()
   end,
 }
