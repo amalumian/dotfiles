bdfea53f5dc0585e99d35eb7af10ab7c372011ef
Arthur Malumian <arthurmalumian@gmail.com>
2025-05-08 03:56:31 +0700

    Update Neovim config

--- nvim/lua/amalumian/plugins/mini.lua
+++ nvim/lua/amalumian/plugins/mini.lua
@@ -4,5 +4,12 @@ return {
     require('mini.ai').setup { n_lines = 500 }
     require('mini.surround').setup()
     require('mini.starter').setup()
+
+    local statusline = require 'mini.statusline'
+    statusline.setup { use_icons = vim.g.have_nerd_font }
+    ---@diagnostic disable-next-line: duplicate-set-field
+    statusline.section_location = function()
+      return '%2l:%-2v'
+    end
   end,
 }
