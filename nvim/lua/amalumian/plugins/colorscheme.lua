766af74c34ce49525fda70401862ba8cb71ffdaf
Arthur Malumian <arthurmalumian@gmail.com>
2025-05-08 03:56:31 +0700

    Update Neovim config

--- nvim/lua/amalumian/plugins/colorscheme.lua
+++ nvim/lua/amalumian/plugins/colorscheme.lua
@@ -1,22 +1,29 @@
 return {
+  -- {
+  --   'sainnhe/gruvbox-material',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd [[
+  --           set background=dark
+  --           let g:gruvbox_material_foreground = 'material'
+  --           let g:gruvbox_material_background = 'medium'
+  --           let g:gruvbox_material_better_performance = 1
+  --           let g:gruvbox_material_enable_italic = 1
+  --           let g:gruvbox_material_inlay_hints_background = 'dimmed'
+  --           let g:gruvbox_material_diagnostic_text_highlight = 1
+  --           let g:gruvbox_material_diagnostic_virtual_text = "colored"
+  --           let g:gruvbox_material_sign_column_background = 'none'
+  --
+  --           colorscheme gruvbox-material
+  --           ]]
+  --   end,
+  -- },
   {
-    'sainnhe/gruvbox-material',
-    lazy = false,
-    priority = 1000,
+    'rose-pine/neovim',
+    name = 'rose-pine',
     config = function()
-      vim.cmd [[
-            set background=dark
-            let g:gruvbox_material_foreground = 'material'
-            let g:gruvbox_material_background = 'medium'
-            let g:gruvbox_material_better_performance = 1
-            let g:gruvbox_material_enable_italic = 1
-            let g:gruvbox_material_inlay_hints_background = 'dimmed'
-            let g:gruvbox_material_diagnostic_text_highlight = 1
-            let g:gruvbox_material_diagnostic_virtual_text = "colored"
-            let g:gruvbox_material_sign_column_background = 'none'
-
-            colorscheme gruvbox-material
-            ]]
+      vim.cmd 'colorscheme rose-pine'
     end,
   },
 }
