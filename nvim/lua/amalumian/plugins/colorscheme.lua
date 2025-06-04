b32d5d558810c11eda15abbaaa69affa3521468b
Arthur Malumian <arthurmalumian@gmail.com>
2025-06-04 23:19:40 +0700

    Setup GitHub Dark theme

--- nvim/lua/amalumian/plugins/colorscheme.lua
+++ nvim/lua/amalumian/plugins/colorscheme.lua
@@ -1,15 +1,17 @@
 return {
   {
-    'rose-pine/neovim',
-    name = 'rose-pine',
+    'projekt0n/github-nvim-theme',
+    name = 'github-theme',
+    lazy = false,
+    priority = 1000,
     config = function()
-      require('rose-pine').setup {
-        styles = {
-          transparency = true,
+      require('github-theme').setup {
+        options = {
+          transparent = true,
         },
       }
 
-      vim.cmd 'colorscheme rose-pine-moon'
+      vim.cmd 'colorscheme github_dark'
     end,
   },
 }
