64322b7eb32d988a2168504320114d01c67cab68
Arthur Malumian <arthurmalumian@gmail.com>
2025-07-26 03:56:01 +0700

    Modify in nvim
    Modified:
      - nvim/lazy-lock.json
      - nvim/lua/config/options.lua

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,6 +4,5 @@ vim.g.snacks_animate = false
 vim.opt.shell = 'zsh'
 vim.opt.guicursor = ''
 vim.opt.mouse = ''
-vim.opt.colorcolumn = '120'
 vim.opt.spell = true
 vim.opt.spelllang = { 'en_us', 'ru' }
