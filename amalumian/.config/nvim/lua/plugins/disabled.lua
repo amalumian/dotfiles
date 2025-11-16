01bca277d9d7e09693af9d3f8b8f5e9f79744c05
Arthur Malumian <arthurmalumian@gmail.com>
2025-11-16 14:52:01 +0700

    Modify in nvim
    Modified:
      - amalumian/.config/nvim/lua/plugins/disabled.lua

--- amalumian/.config/nvim/lua/plugins/disabled.lua
+++ amalumian/.config/nvim/lua/plugins/disabled.lua
@@ -1,12 +1,6 @@
 return {
   { "folke/noice.nvim", enabled = false },
-  {
-    "akinsho/bufferline.nvim",
-    enabled = false,
-  },
-  {
-    "nvim-mini/mini.pairs",
-    enabled = false,
-  },
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "nvim-mini/mini.pairs", enabled = false },
   { "folke/flash.nvim", enabled = false },
 }
