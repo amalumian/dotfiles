610e00e2233b032ab1be15a83cb42e09f6a84028
Arthur Malumian <arthurmalumian@gmail.com>
2025-12-22 15:16:51 +0700

    Modify in nvim, zed, .tmux.conf
    Modified:
      - home/.config/nvim/lua/plugins/disabled.lua
      - home/.config/zed/settings.json
      - home/.tmux.conf

--- home/.config/nvim/lua/plugins/disabled.lua
+++ home/.config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
-  { "folke/noice.nvim", enabled = false },
+  -- { "folke/noice.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-mini/mini.pairs", enabled = false },
   { "folke/flash.nvim", enabled = false },
