392c3f0d5d844d2b1274b80dcb34454f23c656a8
Arthur Malumian <arthurmalumian@gmail.com>
2026-04-08 01:08:04 +0700

    Modify in nvim
    Modified:
      - home/.config/nvim/lazy-lock.json
      - home/.config/nvim/lazyvim.json
      - home/.config/nvim/lua/config/options.lua
      - home/.config/nvim/lua/plugins/lsp/mason.lua
      - home/.config/nvim/stylua.toml

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -1,6 +1,8 @@
 vim.g.autoformat = false
 vim.g.snacks_animate = false
 vim.g.root_spec = { "cwd" }
+
+vim.g.lazyvim_ts_lsp = "tsgo"
 -- vim.g.lazyvim_prettier_needs_config = true
 
 vim.opt.shell = "zsh"
