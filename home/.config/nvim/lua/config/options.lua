cd10c6c8808c29fa2fcdc978f9365acef94e4fd8
Arthur Malumian <arthurmalumian@gmail.com>
2026-04-24 01:16:11 +0700

    Modify in nvim, zed
    Modified:
      - home/.config/nvim/lua/config/options.lua
      - home/.config/zed/settings.json

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -1,9 +1,7 @@
-vim.g.autoformat = false
-vim.g.snacks_animate = false
-vim.g.root_spec = { "cwd" }
-
 vim.opt.shell = "zsh"
 vim.opt.guicursor = ""
 vim.opt.mouse = ""
 
--- vim.g.lazyvim_prettier_needs_config = true
+vim.g.root_spec = { "cwd" }
+vim.g.autoformat = false
+vim.g.snacks_animate = false
