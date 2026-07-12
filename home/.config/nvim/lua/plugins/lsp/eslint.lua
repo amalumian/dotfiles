2dc33635c137ef44b5d61be5e0e866a534440d9e
Arthur Malumian <arthurmalumian@gmail.com>
2026-07-12 20:40:27 +0700

    Modify in nvim
    Modified:
      - home/.config/nvim/lua/plugins/lsp/eslint.lua

--- home/.config/nvim/lua/plugins/lsp/eslint.lua
+++ home/.config/nvim/lua/plugins/lsp/eslint.lua
@@ -1,23 +1,21 @@
--- Add Eslint and use it for formatting
-
 return {
-  -- "neovim/nvim-lspconfig",
-  -- opts = {
-  --   setup = {
-  --     eslint = function()
-  --       local formatter = LazyVim.lsp.formatter({
-  --         name = "eslint: lsp",
-  --         primary = false,
-  --         priority = 200,
-  --         filter = "eslint",
-  --         format = function()
-  --           Snacks.util.lsp.on({ "eslint" }, function()
-  --             vim.cmd("LspEslintFixAll")
-  --           end)
-  --         end,
-  --       })
-  --       LazyVim.format.register(formatter)
-  --     end,
-  --   },
-  -- },
+  "neovim/nvim-lspconfig",
+  opts = {
+    setup = {
+      eslint = function()
+        local formatter = LazyVim.lsp.formatter({
+          name = "eslint: lsp",
+          primary = false,
+          priority = 200,
+          filter = "eslint",
+          format = function()
+            Snacks.util.lsp.on({ "eslint" }, function()
+              vim.cmd("LspEslintFixAll")
+            end)
+          end,
+        })
+        LazyVim.format.register(formatter)
+      end,
+    },
+  },
 }
